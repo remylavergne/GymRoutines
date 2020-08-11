5f36badcf1f84648bf1558220f838af3ef43920e
Noah Jutz <noah.jutz@gmail.com>
2020-08-11 14:17:59 +0200

    Add basic layout

--- app/src/main/java/com/noahjutz/gymroutines/ui/MainActivity.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/MainActivity.kt
@@ -2,10 +2,16 @@ package com.noahjutz.gymroutines.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,10 +21,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                Surface {
-                    Text("Hello World!")
-                }
+                Main()
             }
         }
+    }
+}
+
+@Composable
+fun Main() {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Title") }) },
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Star) },
+                    selected = false,
+                    onSelect = {},
+                    label = { Text("Routines") }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Star) },
+                    selected = false,
+                    onSelect = {},
+                    label = { Text("Exercises") }
+                )
+            }
+        },
+        bodyContent = {}
+    )
+}
+
+@Preview
+@Composable
+fun PreviewMain() {
+    MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
+        Main()
     }
 }
