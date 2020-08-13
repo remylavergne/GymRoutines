4caad29899f44e043fa11978e00a69f075fffb3a
Noah Jutz <noah.jutz@gmail.com>
2020-08-13 11:12:48 +0200

    Add main screen with compose

--- app/src/main/java/com/noahjutz/gymroutines/ui/MainActivity.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.DirectionsRun
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.ViewAgenda
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.state
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,27 +29,39 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+enum class TopLevelDestinations { Routines, Exercises }
+
 @Composable
 fun Main() {
+    val screen = state { TopLevelDestinations.Routines }
     Scaffold(
         topBar = { TopAppBar(title = { Text("Title") }) },
         bottomBar = {
             BottomNavigation {
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.Star) },
-                    selected = false,
-                    onSelect = {},
+                    icon = { Icon(Icons.Filled.ViewAgenda) },
+                    selected = screen.value == TopLevelDestinations.Routines,
+                    onSelect = { screen.value = TopLevelDestinations.Routines },
                     label = { Text("Routines") }
                 )
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.Star) },
-                    selected = false,
-                    onSelect = {},
+                    icon = { Icon(Icons.Filled.DirectionsRun) },
+                    selected = screen.value == TopLevelDestinations.Exercises,
+                    onSelect = { screen.value = TopLevelDestinations.Exercises },
                     label = { Text("Exercises") }
                 )
             }
         },
-        bodyContent = {}
+        bodyContent = {
+            when (screen.value) {
+                TopLevelDestinations.Routines -> {
+                    Text("Routines")
+                }
+                TopLevelDestinations.Exercises -> {
+                    Text("Exercises")
+                }
+            }
+        }
     )
 }
 
