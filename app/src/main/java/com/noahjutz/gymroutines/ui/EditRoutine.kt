ea734cca419b8d48f131bfb4a1fb604fe34c40fb
Noah Jutz <noah.jutz@gmail.com>
2020-08-15 13:00:28 +0200

    Add non-interactive EditRoutine layout
    The layout currently only shows the information on the routine, but
    doesnt allow for editing

--- app/src/main/java/com/noahjutz/gymroutines/ui/EditRoutine.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/EditRoutine.kt
@@ -1,15 +1,21 @@
 package com.noahjutz.gymroutines.ui
 
-import androidx.compose.foundation.Box
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.state
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.noahjutz.gymroutines.data.domain.ExerciseImpl
 import com.noahjutz.gymroutines.data.domain.FullRoutine
 
 @Composable
@@ -29,15 +35,59 @@ fun EditRoutine(
                 }
             )
         },
-        bodyContent = { BodyContent(routine.routine.description) }
+        bodyContent = {
+            BodyContent(
+                name = routine.routine.name,
+                description = routine.routine.description,
+                exercises = routine.exercises
+            )
+        }
     )
 }
 
 @Composable
 private fun BodyContent(
-    description: String
+    name: String,
+    description: String,
+    exercises: List<ExerciseImpl>
 ) {
-    Box(padding = 16.dp) {
-        Text(description)
+    val name = state { name }
+    val description = state { description }
+    Column {
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { Text("Name") },
+            modifier = Modifier.padding(16.dp).fillMaxWidth()
+        )
+        TextField(
+            value = description.value,
+            onValueChange = { description.value = it },
+            label = { Text("Description") },
+            modifier = Modifier.padding(bottom = 16.dp, start = 16.dp, end = 16.dp).fillMaxWidth()
+        )
+        Divider(modifier = Modifier.padding(bottom = 16.dp))
+        LazyColumnFor(
+            items = exercises,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        ) { exercise ->
+            Card(
+                modifier = Modifier.padding(bottom = 16.dp).fillParentMaxWidth()
+            ) {
+                Column(modifier = Modifier.clickable(onClick = {})) {
+                    Text(text = exercise.exercise.name, modifier = Modifier.padding(16.dp))
+                    LazyColumnForIndexed(
+                        items = exercise.sets,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    ) { index, set ->
+                        ListItem(
+                            text = "Set ${index + 1}",
+                            secondaryText = set.toString(),
+                            onClick = {})
+                    }
+                }
+            }
+        }
     }
 }
+
