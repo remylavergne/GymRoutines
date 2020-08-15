0874455cbb672a6988d51304cea7ee79419d200d
Noah Jutz <noah.jutz@gmail.com>
2020-08-15 21:53:13 +0200

    Add layout for EditExercise
    This layout is not interactive, just like EditRoutine. Any changes made
    are not saved to the database

--- app/src/main/java/com/noahjutz/gymroutines/ui/EditExercise.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/EditExercise.kt
@@ -1,14 +1,20 @@
 package com.noahjutz.gymroutines.ui
 
-import androidx.compose.foundation.Box
 import androidx.compose.foundation.Icon
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.state
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.noahjutz.gymroutines.data.domain.Exercise
 
@@ -29,15 +35,55 @@ fun EditExercise(
                 }
             )
         },
-        bodyContent = { BodyContent(exercise.description) }
+        bodyContent = {
+            BodyContent(exercise)
+        }
     )
 }
 
 @Composable
 private fun BodyContent(
-    description: String
+    exercise: Exercise
 ) {
-    Box(padding = 16.dp) {
-        Text(description)
+    val name = state { exercise.name }
+    val description = state { exercise.description }
+
+    val logReps = state { exercise.logReps }
+    val logDistance = state { exercise.logDistance }
+    val logWeight = state { exercise.logWeight }
+    val logTime = state { exercise.logWeight }
+
+    ScrollableColumn {
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { Text("Name") },
+            modifier = Modifier.padding(16.dp).fillMaxWidth()
+        )
+        TextField(
+            value = description.value,
+            onValueChange = { description.value = it },
+            label = { Text("Description") },
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp).fillMaxWidth()
+        )
+        Divider(modifier = Modifier.padding(bottom = 16.dp))
+        LabeledCheckbox(label = "Reps", stateValue = logReps)
+        LabeledCheckbox(label = "Time", stateValue = logTime)
+        LabeledCheckbox(label = "Distance", stateValue = logDistance)
+        LabeledCheckbox(label = "Weight", stateValue = logWeight)
+    }
+}
+
+@Composable
+private fun LabeledCheckbox(label: String, stateValue: MutableState<Boolean>) {
+    Row(
+        modifier = Modifier.clickable(onClick = { stateValue.value = !stateValue.value }).fillMaxWidth()
+    ) {
+        Checkbox(
+            checked = stateValue.value,
+            onCheckedChange = { stateValue.value = it },
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(text = label, modifier = Modifier.gravity(Alignment.CenterVertically))
     }
 }
