017a34eb377d446b69217f02fc7865ad4b2ae8fd
Noah Jutz <noah.jutz@gmail.com>
2020-08-25 11:34:33 +0200

    Rearrange libs

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -18,7 +18,6 @@ object Versions {
     const val material = "1.2.0"
     const val lifecycle = "2.2.0"
 
-    // Test
     const val junit = "4.13"
     const val junitAndroid = "1.1.1"
     const val espresso = "3.2.0"
@@ -26,6 +25,10 @@ object Versions {
 
 object Libs {
     const val core = "androidx.core:core-ktx:1.3.1"
+
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+
+    const val material = "com.google.android.material:material:${Versions.material}"
 
     const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
     const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
@@ -36,8 +39,6 @@ object Libs {
     const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
     const val roomRuntime = "androidx.room:room-runtime:${Versions.room}"
 
-    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
-
     const val compose = "androidx.compose.ui:ui:${Versions.compose}"
     const val composeTooling = "androidx.ui:ui-tooling:${Versions.compose}"
     const val composeFoundation = "androidx.compose.foundation:foundation:${Versions.compose}"
@@ -46,8 +47,6 @@ object Libs {
     const val composeRuntime = "androidx.compose.runtime:runtime:${Versions.compose}"
     const val composeIcons = "androidx.compose.material:material-icons-extended:${Versions.compose}"
     const val composeRouter = "com.github.zsoltk:compose-router:${Versions.composeRouter}"
-
-    const val material = "com.google.android.material:material:${Versions.material}"
 
     const val lifecycle = "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle}"
     const val livedata = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
